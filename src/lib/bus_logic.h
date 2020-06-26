8aa96425b71ed724ebc5aa85e823364bb60a8b39
Steven <solanors20@gmail.com>
2020-06-26 00:45:01 -0600

    Green bus added

--- src/lib/bus_logic.h
+++ src/lib/bus_logic.h
@@ -14,9 +14,18 @@ void init_busRojo() {
     //Periferica Grande - Roja> A1, D1, F2, L4, Z (dandole la vuelta), R2, X5, U5, S6, M1, Y (dandole la vuelta)
 }
 
+//----------------------------------GREEN-BUS
+void init_GreenBus() {
+    int stops[6] = {2, 9, 59, 86, 83, 48};
+    int totalStops = 6;
+    char *name = "GREEN-BUS";
+    add_bus(name, totalStops, stops, 5, 3);
+}
+
 void initBuses()
 {
 	init_busRojo();
+    init_GreenBus();
 }
 
 #endif
