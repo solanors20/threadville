2e1a8375700278f7fa4aa3584ce5c8ce9a216c8e
Jose Pablo Araya <araya.josepablo@gmail.com>
2020-06-26 15:47:53 -0600

    Adding pending buses

--- src/lib/constants.h
+++ src/lib/constants.h
@@ -10,4 +10,16 @@
 #define COLUMNS 52
 #define S 168
 
+#define RED 1
+#define BLUE 2
+#define GREEN 3
+#define BLACK 4
+#define WHITE 5
+#define YELLOW 6
+#define ORANGE 7
+#define GRAY 8
+#define PINK 9
+#define LIGHT_BLUE 10
+
+
 #endif
